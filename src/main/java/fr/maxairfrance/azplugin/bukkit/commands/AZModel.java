fec36cd207008466534fb10a2a7dea67e80cefdd
allo1890 <53498636+allo1890@users.noreply.github.com>
2025-02-07 09:38:01 +0100

    AZplugin

--- src/main/java/fr/maxairfrance/azplugin/bukkit/commands/AZModel.java
+++ src/main/java/fr/maxairfrance/azplugin/bukkit/commands/AZModel.java
@@ -47,6 +47,7 @@ public class AZModel implements AZCommand{
         AZPlayer azPlayer = AZPlugin.getAZManager().getPlayer(target);
         if (args[1].equalsIgnoreCase("reset")){
             azPlayer.getPlayerMeta().setModel(new PactifyModelMetadata(-1));
+            azPlayer.getEntityMeta().setModel(new PactifyModelMetadata(-1));
             azPlayer.updateMeta();
             sender.sendMessage("§a[§2EmauSkin§a] §fChangement de skin effectué !");
             return;
@@ -57,6 +58,7 @@ public class AZModel implements AZCommand{
             PactifyModelMetadata modelMetadata = new PactifyModelMetadata();
             modelMetadata.setId(plspPlayerModel.getId());
             azPlayer.getPlayerMeta().setModel(modelMetadata);
+            azPlayer.getEntityMeta().setModel(modelMetadata);
             Bukkit.getScheduler().runTaskAsynchronously(AZPlugin.getInstance(), azPlayer::updateMeta);
             sender.sendMessage("§a[§2AZPlugin§a]§f Changement de skin effectué !");
         } catch (IllegalArgumentException e){
